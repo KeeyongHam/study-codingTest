8eff044a5ce634970d8c26900eb910b13293944a
Keeyong <ki425125@gmail.com>
2025-07-26 22:47:51 +0900

    Refactor: [1차 프렌즈4블록] 문제풀이 리팩토링

--- src/programmers/level2/프렌즈4블록_1차.java
+++ src/programmers/level2/프렌즈4블록_1차.java
@@ -6,13 +6,17 @@ import java.util.Set;
 
 public class 프렌즈4블록_1차 {
     public int solution(int m, int n, String[] board) {
-
-        Set<Point> matched = new HashSet<>();
         Character[][] boards = createBoards(m, n, board);
+        Set<Point> matched = new HashSet<>();
 
         int result = 0;
         while (true) {
-            findMatchedBlock(m, n, boards, matched);
+            for (int row = 0; row < m - 1; row++) {
+                for (int col = 0; col < n - 1; col++) {
+                    Character cur = boards[row][col];
+                    findMatchedBlock(cur, boards, row, col, matched);
+                }
+            }
 
             if (matched.isEmpty()) {
                 break;
@@ -30,81 +34,88 @@ public class 프렌즈4블록_1차 {
 
     private Character[][] createBoards(int m, int n, String[] board) {
         Character[][] boards = new Character[m][n];
+
         for (int i = 0; i < m; i++) {
-            char[] row = board[i].toCharArray();
+            char[] parsedBlock = board[i].toCharArray();
             for (int j = 0; j < n; j++) {
-                boards[i][j] = row[j];
+                boards[i][j] = parsedBlock[j];
             }
         }
+
         return boards;
     }
 
-    private void findMatchedBlock(int m, int n, Character[][] boards, Set<Point> matched) {
-        for (int i = 0; i < m - 1; i++) {
-            for (int j = 0; j < n - 1; j++) {
-                Character cur = boards[i][j];
+    private void findMatchedBlock(Character cur, Character[][] boards, int row, int col, Set<Point> matched) {
+        if (cur == ' ') {
+            return;
+        }
 
-                if (cur == ' ') {
-                    continue;
-                }
-
-                if (cur == boards[i][j + 1] && cur == boards[i + 1][j] && cur == boards[i + 1][j + 1]) {
-                    matched.add(new Point(i, j));
-                    matched.add(new Point(i, j + 1));
-                    matched.add(new Point(i + 1, j));
-                    matched.add(new Point(i + 1, j + 1));
-                }
-            }
+        if (cur == boards[row][col + 1] && cur == boards[row + 1][col] && cur == boards[row + 1][col + 1]) {
+            matched.add(new Point(row, col));
+            matched.add(new Point(row, col + 1));
+            matched.add(new Point(row + 1, col));
+            matched.add(new Point(row + 1, col + 1));
         }
     }
 
     private void removeBlock(Set<Point> matched, Character[][] boards) {
         for (Point point : matched) {
-            boards[point.getCol()][point.getRow()] = ' ';
+            boards[point.getRow()][point.getCol()] = ' ';
         }
     }
 
     private void applyGravity(int m, int n, Character[][] boards) {
-        for (int j = 0; j < n; j++) {
-            for (int i = m - 1; i >= 0; i--) {
-                if (boards[i][j] == ' ') {
-                    for (int k = i - 1; k >= 0; k--) {
-                        if (boards[k][j] != ' ') {
-                            boards[i][j] = boards[k][j];
-                            boards[k][j] = ' ';
-                            break;
-                        }
-                    }
+        for (int col = 0; col < n; col++) {
+            applyGravityToCol(m, boards, col);
+        }
+    }
+
+    private void applyGravityToCol(int m, Character[][] boards, int col) {
+        for (int emptyRow = m - 1; emptyRow >= 0; emptyRow--) {
+            if (boards[emptyRow][col] != ' ') {
+                continue;
+            }
+
+            for (int fillRow = emptyRow - 1; fillRow >= 0; fillRow--) {
+                if (boards[fillRow][col] != ' ') {
+                    boards[emptyRow][col] = boards[fillRow][col];
+                    boards[fillRow][col] = ' ';
+                    break;
                 }
             }
         }
     }
 
     static class Point {
+
+
         private int row;
         private int col;
 
-        public Point(int col, int row) {
+        public Point(int row, int col) {
             this.col = col;
             this.row = row;
-        }
-
-        public int getCol() {
-            return col;
         }
 
         public int getRow() {
             return row;
         }
 
+        public int getCol() {
+            return col;
+        }
+
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof Point)) return false;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
             Point point = (Point) o;
             return col == point.col && row == point.row;
         }
-
 
         @Override
         public int hashCode() {
