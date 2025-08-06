9cf720aa0d1031a3918990a5cc9347aee0e6c906
Keeyong <ki425125@gmail.com>
2025-08-06 17:26:29 +0900

    Feat: [정수 삼각형] 문제풀이

---
+++ src/programmers/level3/정수_삼각형.java
@@ -0,0 +1,13 @@
+package programmers.level3;
+
+public class 정수_삼각형 {
+    public int solution(int[][] triangle) {
+        for (int i = triangle.length - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                triangle[i][j] = Math.max(triangle[i + 1][j] + triangle[i][j], triangle[i + 1][j + 1] + triangle[i][j]);
+            }
+        }
+
+        return triangle[0][0];
+    }
+}
